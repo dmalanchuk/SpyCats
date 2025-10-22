134178886cf50820f58dc28a5730b55b3251833e
dmalanchuk <marisikdanya@gmail.com>
2025-10-22 19:52:36 +0300

    wrote service and repo for targets

---
+++ src/repositories/targets_repo.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+from sqlalchemy.future import select
+
+from src.models.cats_model import TargetsModel, MissionsModel
+
+class TargetRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_target_by_id(self, target_id: int) -> TargetsModel | None:
+        stmt = (
+            select(TargetsModel)
+            .where(TargetsModel.id == target_id)
+            .options(
+                selectinload(TargetsModel.mission)
+                .selectinload(MissionsModel.targets)
+            )
+        )
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def update_target(self, target: TargetsModel) -> TargetsModel:
+        await self.session.commit()
+        await self.session.refresh(target)
+        return target
---
+++ src/services/targets_service.py
@@ -0,0 +1,39 @@
+from fastapi import HTTPException, status
+from src.repositories.targets_repo import TargetRepository
+from src.schemas.targets_schema import TargetStatus
+from src.models.cats_model import TargetsModel
+
+
+class TargetService:
+    def __init__(self, repo: TargetRepository):
+        self.repo = repo
+
+    async def _get_target_and_check_frozen(self, target_id: int) -> TargetsModel:
+        target = await self.repo.get_target_by_id(target_id)
+        if not target:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, f"Target {target_id} not found.")
+
+        # Перевірка, чи не завершена місія або сама ціль
+        if target.mission.is_complete or target.status == "Completed":
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST,
+                "Cannot update notes or status for a completed target or mission."
+            )
+        return target
+
+    async def update_target_notes(self, target_id: int, notes: str) -> TargetsModel:
+        target = await self._get_target_and_check_frozen(target_id)
+        target.notes = notes
+        return await self.repo.update_target(target)
+
+    async def update_target_status(self, target_id: int, new_status: TargetStatus) -> TargetsModel:
+        target = await self._get_target_and_check_frozen(target_id)
+        target.status = new_status
+
+        all_targets = target.mission.targets
+        is_all_completed = all(t.status == "Completed" for t in all_targets)
+
+        if is_all_completed:
+            target.mission.is_complete = True
+
+        return await self.repo.update_target(target)
