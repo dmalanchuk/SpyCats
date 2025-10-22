8f8ae0c87b9234239d2abfec8bd5a6500016b97e
dmalanchuk <marisikdanya@gmail.com>
2025-10-22 19:20:15 +0300

    created first endpoint for create new mission

--- src/api/missions_api.py
+++ src/api/missions_api.py
@@ -1,21 +1,25 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.database import get_session
+from fastapi import APIRouter, Depends, status
+from src.services.missions_service import MissionService
+from src.schemas.missions_schema import MissionCreate, MissionResponse
+from src.dependencies import get_mission_service
 
 router = APIRouter(prefix="/missions")
 
+
 @router.post(
     "/",
-    summary="Create new spy cats",
-    description="This endpoint allows you to create a new cpy cats",
-    status_code=201,
+    response_model=MissionResponse,
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a new Mission with its Targets",
     responses={
-        201: {"description": "cats created successfully"},
-        400: {"description": "Invalid request"},
+        status.HTTP_201_CREATED: {"description": "Mission created successfully."},
+        status.HTTP_400_BAD_REQUEST: {"description": "Invalid data provided (e.g., duplicate target names)."},
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {"description": "Validation error (e.g., more than 3 targets)."}
     },
 )
-async def create_spy_cats(
-        session: AsyncSession = Depends(get_session)
+async def create_mission_with_targets(
+        mission_data: MissionCreate,
+        mission_service: MissionService = Depends(get_mission_service)
 ):
-    return "hi"
+    new_mission = await mission_service.create_new_mission(mission_data)
+    return new_mission
