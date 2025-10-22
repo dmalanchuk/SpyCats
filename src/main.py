cba7c56b2b03b7fb09dccb2a2a1901c31b48269f
dmalanchuk <marisikdanya@gmail.com>
2025-10-22 18:27:50 +0300

    added two routers

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from src.api.spy_cats_api import router
+from src.api.spy_cats_api import router as router_cats
+from src.api.missions_api import router as router_missions
 
 app = FastAPI(title="Spy Cats")
-app.include_router(router, tags=["spy"])
+app.include_router(router_cats, tags=["cats"])
+app.include_router(router_missions, tags=["missions"])
