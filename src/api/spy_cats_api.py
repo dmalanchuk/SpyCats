04c1426827b0d560d11c02b920ce9cff82229ad7
dmalanchuk <marisikdanya@gmail.com>
2025-10-22 18:57:49 +0300

    wrote two endpoints for getting cats

--- src/api/spy_cats_api.py
+++ src/api/spy_cats_api.py
@@ -23,3 +23,24 @@ async def create_spy_cats(
 ):
     return await cat_service.create_new_cat(cat_data)
 
+
+@router.get(
+    "/",
+    response_model=list[CatResponse],
+    summary="Get all Spy Cats"
+)
+async def get_all_spy_cats(
+    cat_service: CatService = Depends(get_cat_service)
+):
+    return await cat_service.get_all_cats()
+
+@router.get(
+    "/{cat_id}",
+    response_model=CatResponse,
+    summary="Get a single Spy Cat"
+)
+async def get_spy_cat(
+    cat_id: int,
+    cat_service: CatService = Depends(get_cat_service)
+):
+    return await cat_service.get_cat_by_id(cat_id)
