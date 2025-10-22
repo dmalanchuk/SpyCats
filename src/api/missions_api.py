ff88c5c5a2c6b9d27a332ad44a73367e4510dd6d
dmalanchuk <marisikdanya@gmail.com>
2025-10-22 18:27:42 +0300

    added new api for mission and renamed spy cat api

--- src/api/missions_api.py
+++ src/api/missions_api.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_session
 
-router = APIRouter(prefix="/cats")
+router = APIRouter(prefix="/missions")
 
 @router.post(
     "/",
