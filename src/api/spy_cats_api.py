8ca07913f7e0bdd98a925c02eaf3e47cc7b36361
dmalanchuk <marisikdanya@gmail.com>
2025-10-22 12:57:48 +0300

    create file for api crud system

---
+++ src/api/spy_cats_api.py
@@ -0,0 +1,24 @@
+from fastapi import Request
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_session
+
+router = APIRouter(prefix="/spy")
+
+@router.post(
+    "/cats",
+    summary="Create new soy cats",
+    description="This endpoint allows you to create a new cpy cats",
+    status_code=201,
+    responses={
+        201: {"description": "cats created successfully"},
+        400: {"description": "Invalid request"},
+    },
+)
+async def create_spy_cats(
+        request: Request,
+        session: AsyncSession = Depends(get_session)
+):
+    return "hi"
