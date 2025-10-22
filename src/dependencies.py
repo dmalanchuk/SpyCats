ee69ce3811ff7892b3b691e2e7e1f9bcfb27220e
dmalanchuk <marisikdanya@gmail.com>
2025-10-22 18:45:09 +0300

    file for depends

---
+++ src/dependencies.py
@@ -0,0 +1,17 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_session
+
+from src.repositories.cats_repositories import CatRepository
+from src.services.cats_service import CatService
+
+async def get_cat_repository(
+    session: AsyncSession = Depends(get_session)
+) -> CatRepository:
+    return CatRepository(session)
+
+async def get_cat_service(
+    repo: CatRepository = Depends(get_cat_repository)
+) -> CatService:
+    return CatService(repo)
